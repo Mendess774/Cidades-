b768f3981dc76a7ce529b6ad4ddbdb4f606b95fc
Felipe Mendes <you@example.com>
2024-05-26 17:48:02 -0300

    exercírcio finalizado

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Mongaguá");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Peruíbe");
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("Praia Grande");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("São Vicente");
